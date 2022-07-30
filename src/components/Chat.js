c2e8d0ea857c15118ad94cc76c7390ff378f43ec
Manishsingh8135 <manishsingh8135@gmail.com>
2022-07-30 23:37:38 +0530

    front-end last part

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,18 +3,27 @@ import React, { useEffect, useState } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 
 import './Chat.css'
 
 const Chat = () => {
-
+    const [input, setInput]=useState("")
     const [seed, setSeed] = useState("");
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, []);
+
+
+    const sendMessage=(e)=>{
+            e.preventDefault();
+            console.log("Heii",input)
+            setInput("")
+    }
 
 
     return (
@@ -45,8 +54,13 @@ const Chat = () => {
             </div>
 
             <div className='chat__body'>
-                <p className='chat__message'>
+                <p className={`chat__message  ${true && 'chat__reciever'}`}>
+                    <span className='chat__name'>
+                        Elon Musk
+                    </span>
+
                     Hello guys
+                    <span className='time__stamp'>3:32pm</span>
                 </p>
                 <p className='chat__message'>
                     Hello guys
@@ -64,9 +78,17 @@ const Chat = () => {
                     </IconButton>
                 </div>
 
-                <div className='chat__footerSearch'>
-                    <input placeholder='Type here...'></input>
-                </div>
+
+                <form>
+                    <input value={input} onChange={(e) =>setInput(e.target.value)} placeholder='Type here...'></input>
+                    <button 
+                    type='submit' onClick={sendMessage}  className='send__button'><IconButton><SendIcon/></IconButton> </button>
+                </form>
+
+
+                <IconButton>
+                    <MicIcon />
+                </IconButton>
 
 
             </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyBP2cxxPInU2xwCaXYk4GK7YapEGRzK8G4",
     authDomain: "whatsapp-clone-9487b.firebaseapp.com",
@@ -8,3 +9,12 @@ const firebaseConfig = {
     appId: "1:930715413260:web:0db340a889cd9ea77eb729",
     measurementId: "G-9FF4GV42CY"
   }; 
+
+  const firebaseApp= firebase.initializeApp(firebaseConfig)
+
+  const db= firebaseApp.firestore();
+  const auth= firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
